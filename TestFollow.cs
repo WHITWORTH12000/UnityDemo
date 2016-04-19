60d07edc7d621f7b06003e34cb7b04b228927444
WHITWORTH12000 <whitworth12000@gmail.com>
2016-04-19 15:10:18 -0500

    Version 0.2
    In this update for our penguin game, we made it so you have a health bar above your penguin character. This way you can see your health as you go so you know if you are about to die! I hope you guys like the new update to the game.

---
+++ TestFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFollow : MonoBehaviour {
+	public GameObject Player; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player != null) {
+			transform.position = new Vector2 (Player.transform.position.x, Player.transform.position.y + 1f);  
+		}
+	}
+}
